b5180e31453249ce35d3564304dc1ec35bedf943
Khushaal Choithramani <khushaalchoithramani@Khushaals-MacBook-Air.local>
2023-10-20 00:40:58 +0100

    changes

--- versions.sh
+++ versions.sh
@@ -7,6 +7,7 @@ print_menu () {
   echo "Select an operation:"
   echo "[1] Read a Version"
   echo "[2] Rollback a Version"
+  echo "[3] Exit"
 
   read -p "Enter your choice: " choice
 
@@ -17,9 +18,13 @@ print_menu () {
   elif [ "$choice" == "2" ]; then
     echo "$ogPath"
     cat "${folderPath}/${versionSelected}" > "$ogPath"
-    echo "Rollback was succesfull"	
+    echo "Rollback was succesfull"
+  elif [ "$choice" == "3" ]; then
+    echo "Exiting"
+    exit 0 	
   else
     echo "Invalid choice. Please enter 1 or 2."
+    print_menu folderPath versionSelected ogPath 
   fi
 }
 
@@ -33,8 +38,14 @@ root_function () {
     echo "File Found, Select an Operation:"
     echo "Versions Available:"
     ls "$versionsPath"
-    read -p "Enter complete name of the version", versionSelected
-    print_menu $versionsPath $versionSelected $file_path
+    read -p "Enter complete name of the version: " versionSelected
+    if [ -e "$versionsPath/${versionSelected}" ]; then
+    	echo "Version Found"
+   	print_menu $versionsPath $versionSelected $file_path
+    else
+    	echo "Version does not exist"
+        root_function
+     fi
   else
     echo "File does not exist"
   fi
